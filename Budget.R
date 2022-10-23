3aefaf95cd26ce92c53ecd2b454b72fd05a33f79
fabbiocrux <fabbiocrux.ms@gmail.com>
2022-10-23 11:29:30 +0200

    Compilando para la version final

---
+++ Budget.R
@@ -0,0 +1,23 @@
+# Budget Silvia
+Personal = 906170
+Equipement = 115500
+Dissemination = 57200
+Publications = 24000
+
+
+1436088 - (Personal + Equipement + Dissemination + Publications) + 
+   0.25*(Personal + Equipement + Dissemination + Publications)
+
+
+ 
+   
+# Budget Fabio
+
+Fabio =  (5518 * 60)
+Phds = 120000*3
+Postdocs = 3*24*4186
+
+Fabio + Phds + Postdocs
+
+Publications  =  8*3000
+
